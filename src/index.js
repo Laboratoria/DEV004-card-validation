c8e1111147ed45c9c964b214aada7c3329193050
yesseniamillar <yesseniamillar@gmail.com>
2023-02-09 09:22:28 -0300

    isvalid funcional

--- src/index.js
+++ src/index.js
@@ -1,3 +1,20 @@
 import validator from './validator.js';
-
-console.log(validator);
+const buttonValidate = document.getElementById("validate");
+const div = document.getElementById("result");
+buttonValidate.addEventListener("click", function () {
+  const cardNumber = document.getElementById("cardnumber").value;
+  if (cardNumber === null || cardNumber.length < 16 || /^\s+$/.test(cardNumber)) {
+    alert("formato incorrecto");
+  } else {
+    const valid = validator.isValid(cardNumber)
+    if (valid === true) {
+      div.innerHTML = "tarjeta valida";
+    } else {
+      div.innerHTML = "tarjeta invalida";
+    }
+    document.getElementById("cardnumber").value = "";
+  }
+  //const  = validator.maskify(cardNumber)
+})
+console.log(validator);//deberia ser validator.isValid?
+ 
--- src/validator.js
+++ src/validator.js
@@ -1,5 +1,48 @@
 const validator = {
-  // ...
+  isValid:function  (creditCardNumber) {
+    const array = creditCardNumber.split("")
+    const newArray = [];
+    for (let i = 0; i < array.length; i++) {
+      const isEven = i % 2 === 0;
+      if (isEven) {
+        const numberPorDos = Number(array[i]) * 2;
+        if (numberPorDos > 9) {
+          const restaNueve = Number(numberPorDos) - 9;
+          console.log(restaNueve);
+          newArray.push(restaNueve);
+        }
+        else {
+          newArray.push(numberPorDos);// si nuberpordos es menor 9 consignar tal cual. en else. 
+        }
+      }//llave del if inicialde los impares
+      else {
+        newArray.push(Number(array[i]));
+      }
+    }
+    let sumaTotal = 0
+    for (let i = 0; i < newArray.length; i++) {
+      //console.log (newArray[i]);
+      sumaTotal = sumaTotal + newArray[i];
+    }
+    if (sumaTotal % 10 === 0) {
+      return true
+    } else {
+      return false
+    }
+  },
+  //maskify
+  /*maskify:function (creditCardNumber) {
+    //console.log(creditCardNumber)
+    creditCardNumber= creditCardNumber.toString()
+    const recArr = ""; //o [];
+    for (let i = 0; i < creditCardNumber.length; i++) {//o recArr.length
+    //const ?
+    if (i>=-4){
+      recArr = recArr + numero[i]
+    }else{
+      recArr = recArr +  "#" 
+     }
+       return recArr
+  } //*/
 };
-
 export default validator;
